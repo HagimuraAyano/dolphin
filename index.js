ac3f46d5cdfc3b0e1c2f18fbd6466ca056887a69
Hagimura Ayano <hagimuraayano@gmail.com>
2018-11-01 19:33:13 +0800

    fixed typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     netease: require('./lib/netease/'),
-    tencent: require('./lib/netease/'),
+    tencent: require('./lib/tencent/'),
     migu: require('./lib/migu/'),
     xiami: require('./lib/xiami/'),
 }
